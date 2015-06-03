43d6c1fd68d729c4c6eb88fda028e8ddd63e216e
= <=>
2015-06-03 19:29:51 -0400

    Added tabs and fullscreen button to Studio Editor.

--- cdot_slides_for_edx/static/js/cdot_slides_for_edx_studio.js
+++ cdot_slides_for_edx/static/js/cdot_slides_for_edx_studio.js
@@ -2,10 +2,11 @@
 function CDOTSlidesXBlockStudio(runtime, element) {
 
     var isFullscreen = false;
-    var sHeight = 0.88 * $(window).height();
+    var sHeight = 0;
     var sWidth = "70%";
-    var sTop = "0%";
+    var sTop = "15.5%";
     var sLeft = "15%";
+    var csxColor = ["#009FE6", "black"];
 
     // Manually set this to where you store CKEditor
     var CKEditor_URL = "";
@@ -14,7 +15,17 @@ function CDOTSlidesXBlockStudio(runtime, element) {
         lineNumbers: true,
         matchBrackets: true,
         autoCloseBrackets: true,
+        lineWrapping: true,
         theme: "mdn-like"
+    };
+
+    var studio_buttons = {
+        "csx_options": "Options",
+        "csx_preview": "Preview",
+        "csx_html": "HTML",
+        "csx_javascript": "JavaScript",
+        "csx_css": "CSS",
+        "csx_fullscreen": "Fullscreen"
     };
 
     // Attach CKEditor or CodeMirror (as fallback) to HTML input textarea
@@ -35,41 +46,66 @@ function CDOTSlidesXBlockStudio(runtime, element) {
     var editor_tracked = CodeMirror.fromTextArea($('.cdot_body_tracked')[0],
         codemirror_settings
     );
-    editor_tracked.setSize("100%", 120);
 
     var editor_js = CodeMirror.fromTextArea($('.cdot_body_js')[0],
         jQuery.extend({mode: {name: "javascript", globalVars: true}}, codemirror_settings)
     );
-    editor_js.setSize("100%", 600);
 
     var editor_json = CodeMirror.fromTextArea($('.cdot_body_json')[0],
         jQuery.extend({mode: {name: "javascript", globalVars: true, json: true}}, codemirror_settings)
     );
-    editor_json.setSize("100%", 200);
 
     var editor_css = CodeMirror.fromTextArea($('.cdot_body_css')[0],
         jQuery.extend({mode: {name: "css", globalVars: true}}, codemirror_settings)
     );
-    editor_css.setSize("100%", 600);
 
     // Adjust Editor dialog to fit the entire window
     function xblock_maximize() {
         isFullscreen = true;
         $('.modal-window').css({"top": "0px", "left": "0px", "width": "100%"});
-        $('.modal-content').css({"height": sHeight});
+        $('.modal-content').css({"height": 0.865 * $(window).height()});
+        if (CKEditor_URL.endsWith("ckeditor.js")) CKEDITOR.config.height = 600;
+        editor_html.setSize("100%", 0.8 * $(window).height());
+        editor_tracked.setSize("100%", 120);
+        editor_js.setSize("100%", 0.8 * $(window).height());
+        editor_json.setSize("100%", 230);
+        editor_css.setSize("100%", 0.8 * $(window).height());
+        $('#csx_fullscreen').css({"color": csxColor[1]});
     }
 
     // Adjust Editor dialog to edX's standard settings
     function xblock_minimize() {
         isFullscreen = false;
         $('.modal-window').css({"top": sTop, "left": sLeft, "width": sWidth});
-        $('.modal-content').css({"height": sHeight});
+        $('.modal-content').css({"height": 0.6 * $(window).height()});
+        if (CKEditor_URL.endsWith("ckeditor.js")) CKEDITOR.config.height = 600;
+        editor_html.setSize("100%", 370);
+        editor_tracked.setSize("100%", 120);
+        editor_js.setSize("100%", 370);
+        editor_json.setSize("100%", 230);
+        editor_css.setSize("100%", 370);
+        $('#csx_fullscreen').css({"color": csxColor[0]});
     }
 
     // Refresh Editor dimensions
     function xblock_refresh() {
         if (isFullscreen) xblock_maximize();
         else xblock_minimize();
+    }
+
+    function tab_highlight(toHighlight) {
+        for (var b in studio_buttons) {
+            if (b != "csx_fullscreen") $("#" + b).css({"color": csxColor[0]});
+        }
+        $("#" + toHighlight).css({"color": csxColor[1]});
+    }
+
+    // Hide all panes except toShow
+    function tab_switch(toShow) {
+        tab_highlight(toShow);
+        for (var b in studio_buttons) $("." + b).hide();
+        $("." + toShow).show();
+        xblock_refresh();
     }
 
     $(function($) {
@@ -82,54 +118,46 @@ function CDOTSlidesXBlockStudio(runtime, element) {
                 )
             );
 
-        // Add Fullscreen Button
-        $('.editor-modes')
-            .append(
-                $('<li>', {class: "action-item"}).append(
-                    $('<a />', {class: "action-primary", id: "btn_fullscreen", text: "Fullscreen"})
-                )
-            );
-
-        // Readjust settings in case the window is resized
-        window.addEventListener('resize', function(event){
-            if (!isFullscreen) xblock_minimize();
-            else xblock_maximize();
-        });
-
-        // Fill the window with the Editor view
-        $('#btn_fullscreen').click(function() {
-            if (!isFullscreen) xblock_maximize();
-            else xblock_minimize();
-        });
-
-        function toggle_pane(t) {
-            $('.csx_options').hide();
-            $('.csx_html').hide();
-            $('.csx_javascript').hide();
-            $('.csx_css').hide();
-            $('.csx_preview').hide();
-            $(t).show();
-            xblock_refresh();
+        for (var b in studio_buttons) {
+            $('.editor-modes')
+                .append(
+                    $('<li>', {class: "action-item"}).append(
+                        $('<a />', {class: "action-primary", id: b, text: studio_buttons[b]})
+                    )
+                );
         }
 
+        // Set main pane to Options
+        tab_switch("csx_options");
+        // Adjust the modal window
+        xblock_minimize();
+        // Readjust modal window dimensions in case the browser window is resized
+        window.addEventListener('resize', xblock_refresh());
+
         $('#csx_options').click(function() {
-            toggle_pane(".csx_options");
-        });
-
-        $('#csx_html').click(function() {
-            toggle_pane(".csx_html");
-        });
-
-        $('#csx_javascript').click(function() {
-            toggle_pane(".csx_javascript");
-        });
-
-        $('#csx_css').click(function() {
-            toggle_pane(".csx_css");
+            tab_switch("csx_options");
         });
 
         $('#csx_preview').click(function() {
-            toggle_pane(".csx_preview");
+            tab_switch("csx_preview");
+        });
+
+        $('#csx_html').click(function() {
+            tab_switch("csx_html");
+        });
+
+        $('#csx_javascript').click(function() {
+            tab_switch("csx_javascript");
+        });
+
+        $('#csx_css').click(function() {
+            tab_switch("csx_css");
+        });
+
+        // Fill the window with the Editor view
+        $('#csx_fullscreen').click(function() {
+            isFullscreen = !isFullscreen;
+            xblock_refresh();
         });
 
         // Clicked Save button
