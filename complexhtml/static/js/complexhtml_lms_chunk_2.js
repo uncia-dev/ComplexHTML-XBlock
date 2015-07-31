8e05d062102bd2232d5cfc977eb606225fee848d
uw-ray <uw.rayblaga90@gmail.com>
2015-07-31 15:27:36 -0400

    Minor tweaks

--- complexhtml/static/js/complexhtml_lms_chunk_2.js
+++ complexhtml/static/js/complexhtml_lms_chunk_2.js
@@ -2,14 +2,14 @@
 /* Page is loaded. Do something. */
 $(function ($) {
 
-    tick_timer = setInterval(function() {
-            $.ajax({
-                type: "POST",
-                url: runtime.handlerUrl(xblock_element, 'session_tick'),
-                data: JSON.stringify({}),
-                async: false
-            });
-        }, session_tick);
+tick_timer = setInterval(function() {
+        $.ajax({
+            type: "POST",
+            url: runtime.handlerUrl(xblock_element, 'session_tick'),
+            data: JSON.stringify({}),
+            async: false
+        });
+    }, session_tick);
 
-    $(window).unload(function() { session_end(); });
-    $('.chx_end_session').click(function() { session_end(); });
+$(window).unload(function() { session_end(); });
+$('.chx_end_session').click(function() { session_end(); });
